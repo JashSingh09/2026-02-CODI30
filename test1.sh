f4986725fa2133a263930705d8007d4d2f47cbbc
Jashan Singh <jashan0809.singh@gmail.com>
2026-02-11 10:24:40 -0600

    started writing notes

--- test1.sh
+++ test1.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-$number= 1,2,3,4,5,6,7,8,9,10
-
-echo pick a number between 1-10
 #time to get cooked
-	if [[ input =$number ]]; then
-	 	#statements
-	 fi input
+GUESS=$(($RANDOM % 10 + 1))
+#define Variable,
+#$ equals the variable, random is a bash varible that pickes from 0-9, +1 bumps i to 1-10
+ATTEMPTS=0
+#track attempts
+echo "pick a number between 1-10"
+echo 'dont you dare try to pick another number'
+#echo displays the text to the user
+
+while true; do
+#creates infinite loop by repeating the code until number is guessed
+	read -p "Your guess:" GUESS
+# take in user input, -p gives a prompt to a user and stores it in GUESS
+if [[ $GUESS =~ ]]; then
+	#statements
+fiif !
+#checks if the input given is a number input or not
 
 
