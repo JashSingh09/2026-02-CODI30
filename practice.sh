5220e802624062bb9b972e7981e01bb084bbabac
Jashan Singh <jashan0809.singh@gmail.com>
2026-02-24 10:21:53 -0600

    Mr Warren teaches well

--- practice.sh
+++ practice.sh
@@ -4,3 +4,17 @@ variables_are_great="
 "
 foo=14
 tar=sixteen
+
+
+
+
+
+var_a= "One"
+var_b="Two" 
+
+menu_options()(
+	echo -e '
+	Choose an option from below
+	---------------------------
+	|
+	|
