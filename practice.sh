0ae07b8514ee313449715b7aa863daf0b0dd004c
Jashan Singh <jashan0809.singh@gmail.com>
2026-02-24 10:37:48 -0600

    in case i forget my notes

--- practice.sh
+++ practice.sh
@@ -14,7 +14,11 @@ var_b="Two"
 
 menu_options()(
 	echo -e '
-	Choose an option from below
+	|Choose an option from below
 	---------------------------
 	|
 	|
+'
+
+
+#case statements do incase of this, do this option, has a lot of syntax that is useful, lazy version of if statements, you can check simple logic
