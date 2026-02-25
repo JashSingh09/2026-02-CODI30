8e5c4226c7e8577e29ef059fb85c4921ba420c4e
Jashan Singh <jashan0809.singh@gmail.com>
2026-02-25 10:23:09 -0600

    wassup

--- practice.sh
+++ practice.sh
@@ -1,22 +1,25 @@
 #!bin/bash
 
-variables_are_great="
-"
-foo=14
-tar=sixteen
+# variables_are_great="
+# "
+# foo=14
+# tar=sixteen
 
 
-lol_function()()
-var_a= "One"
-var_b="Two" 
+# lol_function()()
+# var_a= "One"
+# var_b="Two" 
 
-menu_options()(
-	echo -e '
-	|Choose an option from below
-	---------------------------
-	|
-	|
-'
-)
+# menu_options()(
+# 	echo -e '
+# 	|Choose an option from below
+# 	---------------------------
+# 	|
+# 	|
+# '
+# )
 
-#case statements do incase of this, do this option, has a lot of syntax that is useful, lazy version of if statements, you can check simple logic
+		#case statements do incase of this, do this option, has a lot of syntax that is useful, lazy version of if statements, you can check simple logic
+
+
+
